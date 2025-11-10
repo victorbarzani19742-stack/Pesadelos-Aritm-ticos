3aac1732a199777e4115ecaec2eca7dfdcb2d4b5
guilhermeCsharp <guilhermemacfaria@icloud.com>
2025-11-10 20:29:09 -0300

    adicionando portais linkando os mapas

---
+++ CENAS/HUD.gd
@@ -0,0 +1,28 @@
+extends CanvasLayer
+signal restart
+
+func show_game_over():
+	visible = true
+	$Message.text = "Você perdeu"
+	$Leave.visible = true
+	$Button.visible = true
+	$Sprite2D.visible = false
+
+func show_victory():
+	visible = true
+	$Message.text = "Você venceu!"
+	$Button.visible = true
+	$Sprite2D.visible = true
+	$Leave.visible = true
+	$Sprite2D.visible = true
+
+func hide_game_ui():
+	visible = false
+
+func _on_button_pressed() -> void:
+	hide_game_ui()
+	get_tree().change_scene_to_file("res://menu.tscn")
+
+
+func _on_leave_pressed() -> void:
+	get_tree().quit()
